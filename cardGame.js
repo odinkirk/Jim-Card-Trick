2607a998f9a0a059553828e0bbf46876604550c2
odinkirk <odinkirk@gmail.com>
2014-06-14 22:23:49 +0000

    Revert "Functionality for deck with user complete, working on the rest"
    This reverts commit 47912fddeb8e1485d11f933bb5d728d8993671ba.

--- cardGame.js
+++ cardGame.js
@@ -10,7 +10,8 @@ function CardController($scope) {
 
     //used for counting the number of cards in manual mode
     //$scope.deckCount = 0;
-   
+    //used for holding a template deck
+    $scope.originalDeckBase = ["SA", "S2", "S3", "S4", "S5", "S6", "S7", "S8", "S9", "S10", "SJ", "SQ", "SK", "DA", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10", "DJ", "DQ", "DK", "CA", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "CJ", "CQ", "CK", "HA", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10", "HJ", "HQ", "HK"];
     //used for holding an orderly deck
     $scope.originalDeck = [];
     // array to contain shuffled deck
